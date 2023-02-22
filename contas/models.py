90f552598eab60beede56d3cd1cfb59eadd4e324
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-02-22 16:38:29 -0300

    Criação do model de transações e registro no site)

--- contas/models.py
+++ contas/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Categoria (models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
+
+
+class Transacao(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length=250)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField()
