94cc8b7b656b34955c62f16ed99dcf2caf1d7491
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-02-22 16:41:52 -0300

    Alteração da exibição da listagem no django-admin

--- contas/models.py
+++ contas/models.py
@@ -5,6 +5,9 @@ class Categoria (models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.nome
+
 
 class Transacao(models.Model):
     data = models.DateTimeField(auto_now_add=True)
@@ -12,3 +15,6 @@ class Transacao(models.Model):
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     observacoes = models.TextField()
+
+    def __str__(self):
+        return self.descricao
